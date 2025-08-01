6960dca3d659c5871edfb85a1e2d5785d2140052
Mendeiros <gmmedeiros@furb.br>
2025-07-31 21:19:56 -0300

    Inicio, aula 31

---
+++ PraticaPOO/src/unidade01/exemplos/App.java
@@ -0,0 +1,24 @@
+package unidade01.exemplos;
+
+public class App {
+    
+    public static void main(String[] args) {
+        Cachorro dog1;
+
+        dog1 = new Cachorro();
+
+        dog1.nome = "Pingo";
+        dog1.raca = "Labrador";
+        dog1.cor = "Branco";
+        dog1.peso = 12.0;
+
+        Cachorro dog2 = new Cachorro();
+
+        dog2.nome = "Brutus";
+        dog2.raca = "Pastor Alemão";
+        dog2.cor = "Marrom";
+        dog2.peso = 21.0;
+
+        
+    }
+}
---
+++ PraticaPOO/src/unidade01/exemplos/Cachorro.java
@@ -0,0 +1,9 @@
+package unidade01.exemplos;
+
+public class Cachorro {
+    
+    String nome;
+    String raca;
+    String cor;
+    Double peso;
+}
